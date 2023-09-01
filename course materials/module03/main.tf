fb8209cf8f4ed99e06444c993081d61a409294a4
Tor Ivar Melling <tor.i.melling@ntnu.no>
2023-09-01 08:47:03 +0200

    update

--- course materials/module03/main.tf
+++ course materials/module03/main.tf
@@ -1,13 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-<<<<<<< HEAD
       source  = "hashicorp/azurerm"
       version = "3.69.0"
-=======
-        source = "hashicorp/azurerm"
-        version = "3.69.0"
->>>>>>> 6a4a078e051c3cf12ba09c0a9c1810039dbe3c01
     }
   }
 }
@@ -16,7 +11,6 @@ provider "azurerm" {
   features {
 
   }
-<<<<<<< HEAD
 }
 
 locals {
@@ -41,6 +35,5 @@ resource "azurerm_storage_account" "sa-demo" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   tags                     = local.tags
-=======
->>>>>>> 6a4a078e051c3cf12ba09c0a9c1810039dbe3c01
+
 }
