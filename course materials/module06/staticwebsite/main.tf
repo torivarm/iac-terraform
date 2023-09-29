7156afd8e26bd4ede41ef62e8634daaa51754bc4
Tor Ivar Melling <tor.i.melling@ntnu.no>
2023-09-29 12:49:52 +0200

    deploy web

--- course materials/module06/staticwebsite/main.tf
+++ course materials/module06/staticwebsite/main.tf
@@ -2,7 +2,7 @@ locals {
   workspace_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
   
   rg_name = terraform.workspace == "default" ? "${var.rg_name}" : "${var.rg_name}-${local.workspace_suffix}"
-  sa_name = "${var.sa_name}${local.workspace_suffix}"
+  sa_name = terraform.workspace == "default" ? "${var.sa_name}" : "${var.sa_name}${local.workspace_suffix}"
   web_suffix = "<h1>${terraform.workspace}</h1>"
 }
 
@@ -24,7 +24,7 @@ resource "azurerm_storage_account" "sa_web" {
   resource_group_name      = azurerm_resource_group.rg_web.name
   location                 = azurerm_resource_group.rg_web.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
 
   static_website {
     index_document = var.index_document
