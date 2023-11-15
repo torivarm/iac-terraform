ffa2c395f27b19f0e62d532edd9b8c5e4640f67f
Tor Ivar Melling <tor.i.melling@ntnu.no>
2023-11-15 10:28:14 +0100

    Add default values to network variables

--- course materials/module06/demo-oblig/network/variables.tf
+++ course materials/module06/demo-oblig/network/variables.tf
@@ -1,24 +1,29 @@
 variable "vnet_rg_name" {
     type = string
     description = "Resource group name"
+    default = "rg-vnet"
 }
 
 variable "vnet_rg_location" {
     type = string
     description = "Resource group location"
+    default = "westeurope"
 }
 
 variable "vnet_name" {
     type = string
     description = "Virtual network name"
+    default = "vnet"
 }
 
 variable "nsg_name" {
     type = string
     description = "Network security group name"
+    default = "nsg"
 }
 
 variable "subnet_name" {
     type = string
     description = "Subnet name"
+    default = "subnet"
 }
