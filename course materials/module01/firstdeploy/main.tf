1d0e0ed88f2ad2b4ed8a5c191fcf7405a0e32e92
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-08-12 10:57:59 +0200

    update

--- course materials/module01/firstdeploy/main.tf
+++ course materials/module01/firstdeploy/main.tf
@@ -1,30 +1,28 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "3.69.0"
+      source = "hashicorp/azurerm"
+      version = "3.115.0"
     }
   }
 }
 
 provider "azurerm" {
   features {
-
+    
   }
 }
 
-resource "azurerm_resource_group" "rg-first" {
-  name = "rg-demo-we-tim"
-  location = "west europe"
+resource "azurerm_resource_group" "fd-rg" {
+  name     = "rg-demo-we" # Use a unique name for the resource group, prefix or suffix with your initials tim-demo-rg-we / rg-demo-we-tim
+  location = "West Europe"
 }
 
 resource "azurerm_storage_account" "sa-demo" {
-  name = "tim12345tim"
-  resource_group_name = azurerm_resource_group.rg-first.name
-  location = azurerm_resource_group.rg-first.location
-  account_tier = "Standard"
-  account_replication_type = "GRS"
+  name                     = "timdemo1334h24"
+  resource_group_name      = azurerm_resource_group.fd-rg.name
+  location                 = azurerm_resource_group.fd-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
-  tags = {
-  }
 }
