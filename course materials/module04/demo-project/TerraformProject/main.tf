526f8933cc0b7b107569a3c3e7e5df60f1cd2b09
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-09-11 10:07:28 +0200

    update

---
+++ course materials/module04/demo-project/TerraformProject/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.0.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "5513747a-818d-4f48-83b0-da2b2fd4cb97"
+  features {
+
+  }
+}
