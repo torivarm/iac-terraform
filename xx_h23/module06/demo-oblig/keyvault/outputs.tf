853081f27e9d1f1d50d4fe1a8175be638753593a
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-09-12 20:24:11 +0200

    update

--- xx_h23/module06/demo-oblig/keyvault/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "keyvault_name" {
-  value = azurerm_key_vault.kv.name
-}
-output "keyvault_id" {
-  value = azurerm_key_vault.kv.id
-}
--- xx_h23/module06/demo-oblig/network/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "subnet_id_output" {
-    value = azurerm_subnet.subnet.id
-}
