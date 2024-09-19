37d8051a1bdb8794055a2ab8201b0f93babb3ed0
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-09-19 09:41:54 +0200

    Update

---
+++ course materials/module05/terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "rg_name" {
+    type = string
+    description = "Name of the resource group"
+}
+
+variable "rg_location" {
+    type = string
+    description = "Location of the resource group"
+}
