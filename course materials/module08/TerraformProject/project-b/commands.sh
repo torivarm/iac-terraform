f5f8b29cfb7c1b9312a34e65c2958b5c3745d6a7
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-10-15 22:46:18 +0200

    update commands.sh

--- course materials/module08/TerraformProject/project-b/commands.sh
+++ course materials/module08/TerraformProject/project-b/commands.sh
@@ -12,6 +12,14 @@ az resource list --resource-type "Microsoft.Storage/storageAccounts" --output ta
 az resource show --resource-group rg-demo-project-b --name sademo12364s --resource-type "Microsoft.Storage/storageAccounts" --query id --output tsv
 
 
+# List tfstate file located in storae account
+az storage blob list --account-name sademobackendtim --container-name tfstate --output table
+
+# Create a json file with the current state file
+az storage blob download --account-name sademobackendtim --container-name tfstate --name project_a.tfstate --file project_a.tfstate
+az storage blob download --account-name sademobackendtim --container-name tfstate --name project_b.tfstate --file project_b.tfstate
+
 # Delete tfstate file from storage account
 az storage blob delete --account-name sademobackendtim --container-name tfstate --name project_a.tfstate
 az storage blob delete --account-name sademobackendtim --container-name tfstate --name project_b.tfstate
+
