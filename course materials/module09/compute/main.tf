f13781288c0d5504b58833788097e52c124959d3
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-10-23 14:09:46 +0200

    update

---
+++ course materials/module09/compute/main.tf
@@ -0,0 +1,110 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-demo-backend-tim"
+    storage_account_name = "sademobackendtim"
+    container_name       = "tfstate"
+    key                  = "compute.tfstate"
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "5513747a-818d-4f48-83b0-da2b2fd4cb97"
+  features {
+    
+  }
+}
+
+# Reference the network state
+data "terraform_remote_state" "network" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "rg-demo-backend-tim"
+    storage_account_name = "sademobackendtim"
+    container_name       = "tfstate"
+    key                 = "network.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "vm" {
+  name     = "vm-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "vm-public-ip"
+  resource_group_name = azurerm_resource_group.vm.name
+  location            = azurerm_resource_group.vm.location
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "vm-nic"
+  location            = azurerm_resource_group.vm.location
+  resource_group_name = azurerm_resource_group.vm.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = data.terraform_remote_state.network.outputs.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id         = azurerm_public_ip.main.id
+  }
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "vm-nsg"
+  location            = azurerm_resource_group.vm.location
+  resource_group_name = azurerm_resource_group.vm.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "simple-vm"
+  resource_group_name             = azurerm_resource_group.vm.name
+  location                        = azurerm_resource_group.vm.location
+  size                           = "Standard_B1s"
+  admin_username                  = "adminuser"
+  admin_password                  = "P@ssw0rd1234!"  # In production, use variables and key vault
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.main.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+output "public_ip_address" {
+  value = azurerm_public_ip.main.ip_address
+}
---
+++ course materials/module09/network/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-demo-backend-tim"
+    storage_account_name = "sademobackendtim"
+    container_name       = "tfstate"
+    key                  = "network.tfstate"
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "5513747a-818d-4f48-83b0-da2b2fd4cb97"
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "network" {
+  name     = "network-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "main-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.network.location
+  resource_group_name = azurerm_resource_group.network.name
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.network.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+output "subnet_id" {
+  value = azurerm_subnet.main.id
+}
