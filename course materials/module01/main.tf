54a2bc34dc4d28cd70ef5680e13531bbaa2b2337
Tor Ivar Melling <tor.i.melling@ntnu.no>
2025-08-19 10:33:49 +0200

    update

--- course materials/module01/main.tf
+++ course materials/module01/main.tf
@@ -9,25 +9,26 @@ terraform {
 
 provider "azurerm" {
   # Configuration options
-  subscription_id = "a3adf20e-4966-4afb-b717-4de1baae6db1"
   features {
 
   }
+  subscription_id = "a3adf20e-4966-4afb-b717-4de1baae6db1"
 }
 
+
 resource "azurerm_resource_group" "rg-sa" {
-  name     = "rg-demo-we-tim"
+  name     = "example-resources"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "sdaaa123timadsf"
+  name                     = "timdmsdfdsf234wrref"
   resource_group_name      = azurerm_resource_group.rg-sa.name
   location                 = azurerm_resource_group.rg-sa.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
 
   tags = {
-    environment = "test"
+    environment = "staging"
   }
 }
