38461fcfe8b0bc070f88ca56779699524b82d52e
Tor Ivar Melling <tor.i.melling@ntnu.no>
2025-09-24 14:43:17 +0200

    update

--- course materials/module06/terraform-projects/rg/main.tf
+++ course materials/module06/terraform-projects/rg/main.tf
@@ -5,8 +5,6 @@ terraform {
       version = "~> 4.40.0"
     }
   }
-  backend "azurerm" {
-  }
 }
 
 provider "azurerm" {
@@ -14,27 +12,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "fd-rg" {
-  name     = var.rg_name
-  location = var.location
+  name     = "rg-demo-sf34123"
+  location = "West Europe"
 }
 
-resource "azurerm_storage_account" "sa_demo" {
-    name                     = "sademonametim12357"
-    resource_group_name      = azurerm_resource_group.fd-rg.name
-    location                 = azurerm_resource_group.fd-rg.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
-}
-
-
-### Input variables ###
-
-variable "rg_name" {
-  description = "Name of the Resource Group"
-  type        = string
-}
-
-variable "location" {
-  description = "Azure location for the Resource Group"
-  type        = string
-}
